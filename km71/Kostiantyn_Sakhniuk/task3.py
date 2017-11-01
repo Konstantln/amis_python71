2fa04450153042140be392ce95fa7c59d6845dc8
Konstiantyn <32033941+Konstantln@users.noreply.github.com>
2017-11-01 19:50:20 +0200

    3
    Тема домашнього завдання: Поняття змінної та типу даних у Python

--- km71/Kostiantyn_Sakhniuk/task3.py
+++ km71/Kostiantyn_Sakhniuk/task3.py
@@ -1,12 +1,3 @@
 print("Сахнюк Костянтин Олександрович \nKM-71")
-x = int(input("Введіть перше ціле число"))
-y = int(input("Введіть друге ціле число"))
-z = int(input("Введіть третє ціле число"))
-if x < y and x < z:
- print(x)
-elif y < x and y < z:
- print(y)
-elif z < x and z < y:
- print(z)
-else:
- print("Числа однакові")
+Name = input("Введіть своє ім'я")   #Введення імені
+print("Hello,", Name)   #Виведення привітання на екран
