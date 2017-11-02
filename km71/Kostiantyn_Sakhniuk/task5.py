78f9af759719d7b4a960bfade9ccaa170d8aa3b9
Konstiantyn <32033941+Konstantln@users.noreply.github.com>
2017-11-02 17:11:54 +0200

    Delete task5.py

--- km71/Kostiantyn_Sakhniuk/task5.py
+++
@@ -1,4 +0,0 @@
-print("Сахнюк Костянтин Олександрович \nKM-71")
-a = int(input("Enter your number"))   #Введення числа
-print("The next number for the number", a, "is", a + 1)   #Виведення на єкран числа, більшого за a на 1
-print("The previous number for the number", a, "is", a - 1)   #Виведення на єкран числа, меншого за a на 1
