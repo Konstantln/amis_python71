5ea3430de1b72f14aa0c37618acb3d493f6278b4
Konstiantyn <32033941+Konstantln@users.noreply.github.com>
2017-12-22 00:10:18 +0200

    Delete task1.py

--- km71/Kostiantyn_Sakhniuk/9/task1.py
+++
@@ -1,8 +0,0 @@
-def distance(x1, y1, x2, y2):
-    c = ((y1-y2)**2 + (x1 - x2)**2)**(1/2)
-    return c
-x1 = float(input("Введіть перше дійсне число"))
-y1 = float(input("Введіть друге дійсне число"))
-x2 = float(input("Введіть третє дійсне число"))
-y2 = float(input("Введіть четверте дійсне число"))
-print(distance(x1, y1, x2, y2))
